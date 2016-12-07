3c1bb64419ef28f240dcf7a87dc6631776e7a5ad
emalyshev <emalyshev@wellink.ru>
2016-12-07 15:53:14 +0200

    Implemented image

--- app/src/main/java/ua/meugen/android/levelup/homework3/MainActivity.java
+++ app/src/main/java/ua/meugen/android/levelup/homework3/MainActivity.java
@@ -2,12 +2,35 @@ package ua.meugen.android.levelup.homework3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import ua.meugen.android.levelup.homework3.views.TransformationContentView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TransformationContentView contentView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.first_transformation).setOnClickListener(this);
+        findViewById(R.id.second_transformation).setOnClickListener(this);
+        findViewById(R.id.third_transformation).setOnClickListener(this);
+
+        this.contentView = (TransformationContentView) findViewById(R.id.content);
+    }
+
+    @Override
+    public void onClick(final View view) {
+        final int viewId = view.getId();
+        if (viewId == R.id.first_transformation) {
+            this.contentView.setTransformation(TransformationContentView.INVERT_GREEN_AND_BLUE);
+        } else if (viewId == R.id.second_transformation) {
+            this.contentView.setTransformation(TransformationContentView.CUT_IMAGE);
+        } else if (viewId == R.id.third_transformation) {
+            this.contentView.setTransformation(TransformationContentView.PUT_GRADIENT);
+        }
     }
 }
---
+++ app/src/main/java/ua/meugen/android/levelup/homework3/views/TransformationContentView.java
@@ -0,0 +1,112 @@
+package ua.meugen.android.levelup.homework3.views;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+import ua.meugen.android.levelup.homework3.R;
+
+/**
+ * Created by meugen on 06.12.16.
+ */
+
+public class TransformationContentView extends View {
+
+    private static final String TAG = TransformationContentView.class.getSimpleName();
+
+    public static final int NONE = 0;
+    public static final int INVERT_GREEN_AND_BLUE = 1;
+    public static final int CUT_IMAGE = 2;
+    public static final int PUT_GRADIENT = 3;
+
+    private final Paint paint = new Paint();
+    private final RectF bitmapRect = new RectF();
+
+    private Drawable image;
+    private int transformation = NONE;
+
+    public TransformationContentView(final Context context) {
+        super(context);
+    }
+
+    public TransformationContentView(final Context context, final AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public TransformationContentView(final Context context, final AttributeSet attrs,
+                                     final int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        final TypedArray array = context.obtainStyledAttributes(
+                attrs,
+                R.styleable.TransformationContentView,
+                defStyleAttr, 0);
+        this.image = array.getDrawable(R.styleable.TransformationContentView_image);
+        this.transformation = array.getInt(R.styleable.TransformationContentView_transformation, NONE);
+        array.recycle();
+    }
+
+    public Drawable getImage() {
+        return image;
+    }
+
+    public void setImage(final Drawable image) {
+        this.image = image;
+        invalidate();
+    }
+
+    public int getTransformation() {
+        return transformation;
+    }
+
+    public void setTransformation(final int transformation) {
+        this.transformation = transformation;
+        invalidate();
+    }
+
+    @Override
+    protected void onLayout(final boolean changed, final int left,
+                            final int top, final int right,
+                            final int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+
+        if (this.image == null) {
+            this.bitmapRect.set(0, 0, 0, 0);
+        } else {
+            final int width = right - left;
+            final int height = bottom - top;
+
+            final Bitmap bitmap = ((BitmapDrawable) this.image).getBitmap();
+            final float scale = Math.min((float) width / bitmap.getWidth(),
+                    (float) height / bitmap.getHeight());
+            final float scaledWidth = bitmap.getWidth() * scale;
+            final float scaleHeight = bitmap.getHeight() * scale;
+
+            final float verticalPadding = (height - scaleHeight) / 2;
+            final float horizontalPadding = (width - scaledWidth) / 2;
+            this.bitmapRect.set(horizontalPadding, verticalPadding,
+                    width - horizontalPadding, height - verticalPadding);
+        }
+    }
+
+    @Override
+    protected void onDraw(final Canvas canvas) {
+        super.onDraw(canvas);
+        if (this.image == null) {
+            return;
+        }
+        final Bitmap bitmap = ((BitmapDrawable) this.image).getBitmap();
+
+        canvas.drawBitmap(bitmap, null, bitmapRect, this.paint);
+        Log.i(TAG, "Bitmap width: " + bitmap.getWidth() + " and height: " + bitmap.getHeight());
+    }
+}
