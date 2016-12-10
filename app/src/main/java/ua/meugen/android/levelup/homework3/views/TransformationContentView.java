a239146c12752c8e9ab6f7a843cf91a5f5489368
meugen <meugen.82@gmail.com>
2016-12-10 10:16:34 +0200

    Implemented put gradient

--- app/src/main/java/ua/meugen/android/levelup/homework3/views/TransformationContentView.java
+++ app/src/main/java/ua/meugen/android/levelup/homework3/views/TransformationContentView.java
@@ -11,11 +11,15 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
+import android.graphics.RadialGradient;
 import android.graphics.Rect;
 import android.graphics.RectF;
+import android.graphics.Shader;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -27,14 +31,11 @@ import ua.meugen.android.levelup.homework3.R;
 
 public class TransformationContentView extends View {
 
-    private static final String TAG = TransformationContentView.class.getSimpleName();
-
     public static final int NONE = 0;
     public static final int INVERT_GREEN_AND_BLUE = 1;
     public static final int CUT_IMAGE = 2;
     public static final int PUT_GRADIENT = 3;
 
-    private final Paint paint = new Paint();
     private final RectF bitmapRect = new RectF();
 
     private Drawable image;
@@ -75,15 +76,7 @@ public class TransformationContentView extends View {
         } else {
             this.drawHelper = new DrawHelper();
         }
-    }
-
-    public Drawable getImage() {
-        return image;
-    }
-
-    public void setImage(final Drawable image) {
-        this.image = image;
-        invalidate();
+        this.drawHelper.sizeChanged(bitmapRect);
     }
 
     public int getTransformation() {
@@ -97,28 +90,24 @@ public class TransformationContentView extends View {
     }
 
     @Override
-    protected void onLayout(final boolean changed, final int left,
-                            final int top, final int right,
-                            final int bottom) {
-        super.onLayout(changed, left, top, right, bottom);
+    protected void onSizeChanged(final int w, final int h, final int oldw, final int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
 
         if (this.image == null) {
             this.bitmapRect.set(0, 0, 0, 0);
         } else {
-            final int width = right - left;
-            final int height = bottom - top;
-
             final Bitmap bitmap = ((BitmapDrawable) this.image).getBitmap();
-            final float scale = Math.min((float) width / bitmap.getWidth(),
-                    (float) height / bitmap.getHeight());
+            final float scale = Math.min((float) w / bitmap.getWidth(),
+                    (float) h / bitmap.getHeight());
             final float scaledWidth = bitmap.getWidth() * scale;
             final float scaledHeight = bitmap.getHeight() * scale;
 
-            final float verticalPadding = (height - scaledHeight) / 2;
-            final float horizontalPadding = (width - scaledWidth) / 2;
+            final float verticalPadding = (h - scaledHeight) / 2;
+            final float horizontalPadding = (w - scaledWidth) / 2;
             this.bitmapRect.set(horizontalPadding, verticalPadding,
-                    width - horizontalPadding, height - verticalPadding);
+                    w - horizontalPadding, h - verticalPadding);
         }
+        this.drawHelper.sizeChanged(this.bitmapRect);
     }
 
     @Override
@@ -131,79 +120,151 @@ public class TransformationContentView extends View {
         this.drawHelper.draw(canvas, bitmap);
     }
 
-    private class DrawHelper {
-
-        public void draw(final Canvas canvas, final Bitmap bitmap) {
-            canvas.drawBitmap(bitmap, null, bitmapRect, paint);
-        }
+    @Override
+    protected Parcelable onSaveInstanceState() {
+        return new Parcelable_(super.onSaveInstanceState(), this.transformation);
     }
 
-    private class InvertGreenAndBlueDrawHelper extends DrawHelper {
+    @Override
+    protected void onRestoreInstanceState(final Parcelable state) {
+        final Parcelable_ parcelable = (Parcelable_) state;
+        super.onRestoreInstanceState(parcelable.getBase());
+        setTransformation(parcelable.getTransformation());
+    }
 
-        private final float[] colorMatrixArray = new float[] {
-                1,  0,  0, 0,   0, // the same red channel
-                0, -1,  0, 0, 255, // invert green channel
-                0,  0, -1, 0, 255, // invert blue channel
-                0,  0,  0, 1,   0  // the same alpha channel
+    public static final class Parcelable_ implements Parcelable {
+
+        public static final Creator<Parcelable_> CREATOR = new ClassLoaderCreator<Parcelable_>() {
+            @Override
+            public Parcelable_ createFromParcel(final Parcel parcel, final ClassLoader classLoader) {
+                return new Parcelable_(parcel, classLoader);
+            }
+
+            @Override
+            public Parcelable_ createFromParcel(final Parcel parcel) {
+                return createFromParcel(parcel, TransformationContentView.class.getClassLoader());
+            }
+
+            @Override
+            public Parcelable_[] newArray(final int size) {
+                return new Parcelable_[size];
+            }
         };
-        private final ColorMatrixColorFilter colorFilter
-                = new ColorMatrixColorFilter(colorMatrixArray);
+
+        private final Parcelable base;
+        private final int transformation;
+
+        public Parcelable_(final Parcel parcel, final ClassLoader classLoader) {
+            this.base = parcel.readParcelable(classLoader);
+            this.transformation = parcel.readInt();
+        }
+
+        public Parcelable_(final Parcelable base, final int transformation) {
+            this.base = base;
+            this.transformation = transformation;
+        }
+
+        public Parcelable getBase() {
+            return base;
+        }
+
+        public int getTransformation() {
+            return transformation;
+        }
 
         @Override
-        public void draw(final Canvas canvas, final Bitmap bitmap) {
-            paint.setColorFilter(colorFilter);
-            super.draw(canvas, bitmap);
-            paint.setColorFilter(null);
+        public int describeContents() {
+            return 0;
+        }
+
+        @Override
+        public void writeToParcel(final Parcel parcel, final int flags) {
+            parcel.writeParcelable(this.base, flags);
+            parcel.writeInt(this.transformation);
+        }
+    }
+}
+
+class DrawHelper {
+
+    final Paint paint = new Paint();
+    RectF rect;
+
+    public void sizeChanged(final RectF rect) {
+        this.rect = rect;
+    }
+
+    public void draw(final Canvas canvas, final Bitmap bitmap) {
+        canvas.drawBitmap(bitmap, null, rect, paint);
+    }
+}
+
+class InvertGreenAndBlueDrawHelper extends DrawHelper {
+
+    private final float[] colorMatrixArray = new float[] {
+            1,  0,  0, 0,   0, // the same red channel
+            0, -1,  0, 0, 255, // invert green channel
+            0,  0, -1, 0, 255, // invert blue channel
+            0,  0,  0, 1,   0  // the same alpha channel
+    };
+    private final ColorMatrixColorFilter colorFilter
+            = new ColorMatrixColorFilter(colorMatrixArray);
+
+    @Override
+    public void draw(final Canvas canvas, final Bitmap bitmap) {
+        paint.setColorFilter(colorFilter);
+        super.draw(canvas, bitmap);
+        paint.setColorFilter(null);
+    }
+}
+
+class CutImageDrawHelper extends DrawHelper {
+
+    private final Matrix matrix = new Matrix();
+    private final Path path = new Path();
+
+    @Override
+    public void draw(final Canvas canvas, final Bitmap bitmap) {
+        this.matrix.setRotate(40f, rect.centerX(), rect.centerY());
+
+        this.path.reset();
+        this.path.moveTo(rect.centerX(), rect.top);
+        this.path.rLineTo(rect.width() / 2, rect.height() * 2 / 3);
+        this.path.rLineTo(-rect.width(), 0);
+        this.path.lineTo(rect.centerX(), rect.top);
+        this.path.transform(this.matrix);
+
+        canvas.save();
+        canvas.clipPath(this.path);
+        super.draw(canvas, bitmap);
+        canvas.restore();
+    }
+}
+
+class PutGradientDrawHelper extends DrawHelper {
+
+    private RadialGradient gradient;
+
+    @Override
+    public void sizeChanged(final RectF rect) {
+        super.sizeChanged(rect);
+        if (!rect.isEmpty()) {
+            this.gradient = new RadialGradient(rect.centerX(), rect.centerY(),
+                    rect.width() / 2,
+                    new int[] {Color.CYAN, Color.MAGENTA, Color.YELLOW },
+                    null, Shader.TileMode.REPEAT);
         }
     }
 
-    private class CutImageDrawHelper extends DrawHelper {
+    @Override
+    public void draw(final Canvas canvas, final Bitmap bitmap) {
+        super.draw(canvas, bitmap);
 
-        private final Matrix matrix = new Matrix();
-        private final Path path = new Path();
-
-        @Override
-        public void draw(final Canvas canvas, final Bitmap bitmap) {
-            this.matrix.setRotate(40f, bitmapRect.centerX(), bitmapRect.centerY());
-
-            this.path.reset();
-            this.path.moveTo(bitmapRect.centerX(), bitmapRect.top);
-            this.path.rLineTo(bitmapRect.width() / 2, bitmapRect.height() * 2 / 3);
-            this.path.rLineTo(-bitmapRect.width(), 0);
-            this.path.lineTo(bitmapRect.centerX(), bitmapRect.top);
-            this.path.transform(this.matrix);
-
-            canvas.save();
-            canvas.clipPath(this.path);
-            super.draw(canvas, bitmap);
-            canvas.restore();
-        }
-    }
-
-    private class PutGradientDrawHelper extends DrawHelper {
-
-        private final GradientDrawable gradient;
-        private final PorterDuffXfermode xfermode
-                = new PorterDuffXfermode(PorterDuff.Mode.MULTIPLY);
-        private final Rect gradientRect = new Rect();
-
-        public PutGradientDrawHelper() {
-            this.gradient = new GradientDrawable(GradientDrawable.Orientation.BL_TR,
-                    new int[] {Color.CYAN, Color.MAGENTA, Color.YELLOW, Color.TRANSPARENT });
-            this.gradient.setGradientType(GradientDrawable.RADIAL_GRADIENT);
-        }
-
-        @Override
-        public void draw(final Canvas canvas, final Bitmap bitmap) {
-            this.gradient.setGradientRadius(Math.max(bitmapRect.width(), bitmapRect.height()) / 2);
-            this.gradientRect.set(Math.round(bitmapRect.left), Math.round(bitmapRect.top),
-                    Math.round(bitmapRect.right), Math.round(bitmapRect.bottom));
-            this.gradient.setBounds(this.gradientRect);
-
-            paint.setXfermode(this.xfermode);
-            super.draw(canvas, bitmap);
-            this.gradient.draw(canvas);
-            paint.setXfermode(null);
-        }
+        paint.setFakeBoldText(true);
+        paint.setTextSize(300);
+        paint.setShader(gradient);
+        paint.setTextAlign(Paint.Align.CENTER);
+        canvas.drawText("Android", rect.centerX(), rect.centerY(), paint);
+        paint.setShader(null);
     }
 }
